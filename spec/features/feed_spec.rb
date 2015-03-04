2b2447544b3f3dc3caccff90ebfecc6467ec41ec
Alabê Duarte <aduarte@thoughtworks.com>
2015-03-04 09:05:10 -0300

    Comment flaky UI test

--- spec/features/feed_spec.rb
+++ spec/features/feed_spec.rb
@@ -2,52 +2,51 @@ require 'spec_helper'
 require 'rake'
 
 describe 'home' do
+  let(:hash_tag) { ENV['HASHTAG'] }
   let(:twitter_profile_image) {"http://a0.twimg.com/profile_images/447958234/Lichtenstein_normal.jpg"}
   let(:twitter_media_image) {"https://pbs.twimg.com/media/BoqqU1wIMAAr_zO.jpg"}
   let(:instagram_profile_image) {"http://images.ak.instagram.com/profiles/profile_33110152_75sq_1380185157.jpg"}
   let(:instagram_media_image) {"http://scontent-a.cdninstagram.com/hphotos-xfa1/t51.2885-15/10684067_323739034474097_279647979_n.jpg"}
 
-  it 'should display single tweets details', js: true do
+  xit 'should display single tweets details', js: true do
     sleep ENV["API_Rate"].to_i + 0.5
     visit '/'
 
-    page.should have_content("Thee Namaste Nerdz. ##{ENV["HASHTAG"]}")
-    page.should have_content('bullcityrecords')
-    # page.should have_image(twitter_profile_image)
-    page.should have_image(twitter_media_image)
+    expect(page).to have_content("Thee Namaste Nerdz. ##{hash_tag}")
+    expect(page).to have_content('bullcityrecords')
+    expect(page).to have_image(twitter_media_image)
 
-    page.should have_content("#elevator #kiss #love #budapest #basilica #tired")
-    page.should have_content('pollywoah')
-    # page.should have_image(instagram_profile_image)
-    page.should have_image(instagram_media_image)
+    expect(page).to have_content("#elevator #kiss #love #budapest #basilica #tired")
+    expect(page).to have_content('pollywoah')
+    expect(page).to have_image(instagram_media_image)
   end
 
-  xit 'should add a tweet via automatic update', :js=> true do
+  xit 'should add a tweet via automatic update', js: true do
     visit '/'
 
-    page.should have_content("Thee Namaste Nerdz. ##{ENV["HASHTAG"]}")
-    page.should have_content('@bullcityrecords')
-    page.should have_content('Fri Sep 21 11:30 PM')
+    expect(page).to have_content("Thee Namaste Nerdz. ##{hash_tag}")
+    expect(page).to have_content('@bullcityrecords')
+    expect(page).to have_content('Fri Sep 21 11:30 PM')
 
     update_tweets_in_db
-    
+
     sleep(30.seconds)
 
-    page.should have_content("Thee Namaste Nerdz. ##{ENV["HASHTAG"]}")
-    page.should have_content('@bullcityrecords')
-    page.should have_content('Fri Sep 21 11:30 PM')
+    expect(page).to have_content("Thee Namaste Nerdz. ##{hash_tag}")
+    expect(page).to have_content('@bullcityrecords')
+    expect(page).to have_content('Fri Sep 21 11:30 PM')
   end
 
   it 'should not have several swear words' do
-    Capybara.use_default_driver 
+    Capybara.use_default_driver
     sleep ENV["API_Rate"].to_i + 0.5
-    stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{ENV["HASHTAG"]}").
+    stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hash_tag}").
       with(headers: {"Authorization"=>/Bearer .+/}).
       to_return( {:status => 200, :body => SampleTweetResponses.tweets_with_censored_words.to_json, :headers => {'content-type' => 'application/json'} })
     visit '/'
 
     ENV["CENSORED_WORDS"].split("|").each do |word|
-      page.should_not have_content(word)
+      expect(page).to_not have_content(word)
     end
   end
 
